a45ad73bbf867d9e80049a0409b3337334e73c3a
ttuluk <ttasya.k@gmail.com>
2021-09-20 01:40:01 +0300

    add debounce

--- src/partials/js/fetchCountries.js
+++ src/partials/js/fetchCountries.js
@@ -1,39 +1,26 @@
 console.log('hi');
-// const NAME = 'Ukraine';
+// const NAME = 'Ukraine';'
 import countryCard from '../../templates/country-card.hbs';
 import API from './api-service.js'
 import getRefs from './get-refs.js';
+import debounce from 'lodash/debounce'
+import { error } from '@pnotify/core';
 const refs = getRefs();
 const DELAY = 500;
 
-refs.searchForm.addEventListener('input', onSearch);
+refs.searchForm.addEventListener('input', debounce(onSearch, DELAY));
 
 function onSearch(e) {
     e.preventDefault();
-
-const form = e.currentTarget;
-const searchNAME = form.elements.query.value;
-    
-const api = API.fetchCountry(searchNAME).then(renderCountryCard).catch(onFetchError).finally(() => { });
-debounce(api);
-}
-
-function debounce(callback) {
-
-  return function(ar) {
-    clearTimeout(timeout);
-    timeout = setTimeout(callback, DELAY, ar);
-  };
-}
+    const form = e.target.form;
+const searchNAME = form.query.value;  
+    API.fetchCountry(searchNAME).then(renderCountryCard).catch({error}).finally(() => form.reset());
+};
 
 function renderCountryCard(country) {
  const result = country.map(element => {
     return countryCard(element);
     });
     refs.cardContainer.innerHTML = result;
-}
+};
 
-
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так!');
-}
