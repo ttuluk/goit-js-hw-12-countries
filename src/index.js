18d028b7e201c07bad6eb90ec9a1ad29462bcd8f
ttuluk <ttasya.k@gmail.com>
2021-09-19 23:32:54 +0300

    create serch of country

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/main.scss';
+
 import './partials/js/fetchCountries.js';
--- src/partials/js/fetchCountries.js
+++ src/partials/js/fetchCountries.js
@@ -1,10 +1,48 @@
 console.log('hi');
-const NAME = 'Uk';
-let country = fetch(`https://restcountries.eu/rest/v2/name/${NAME}?fields=name;capital`).then(r => {
-    if (r.ok) {
-        return r.json();
-    }
-    return { countries:[]};
-}).then(ar => {
-    console.log(ar);
-});
+// const NAME = 'Ukraine';
+import countryCard from '../../templates/country-card.hbs';
+import API from './api-service.js'
+import getRefs from './get-refs.js';
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchNAME = form.elements.query.value;
+
+ API.fetchCountry(searchNAME).then(renderCountryCard).catch(onFetchError).finally(() => form.reset());
+}
+
+
+// API.fetchCountry(NAME).then(renderCountryCard).catch(onFetchError);
+//     // .finally(() => form.reset());
+
+
+// let country = fetch(`https://restcountries.eu/rest/v2/name/${NAME}`).then(r => {
+//     if (r.ok) {
+//         return r.json();
+//     }
+//     return { countries:[]};
+// }).then(ar => {
+//     console.log(ar);
+//   renderCountryCard({ar});
+// });
+// console.log(country);
+
+
+
+function renderCountryCard(country) {
+ const result = country.map(element => {
+    return countryCard(element);
+     console.log(countryCard(element));
+    });
+    refs.cardContainer.innerHTML = result;
+}
+
+
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так!');
+}
